bc785d7a6e40a96a1d83ef43105e06d077d8ff6b
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-07-31 13:19:56 +0200

    release 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
 plugins {
-    plugins {
-        kotlin("multiplatform") version "2.2.0" apply false
-        kotlin("plugin.serialization") version "2.2.0" apply false
-        id("com.google.devtools.ksp") version "2.2.0-2.0.2"
-        id("io.kotest") version "6.0.0.M6"
-        id("at.asitplus.gradle.conventions") version "20250728"
-    }
-
+    kotlin("multiplatform") version "2.2.0" apply false
+    kotlin("plugin.serialization") version "2.2.0" apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
+    id("io.kotest") version "6.0.0.M6"
+    id("at.asitplus.gradle.conventions") version "20250728"
 }
 
 val artifactVersion: String by extra
